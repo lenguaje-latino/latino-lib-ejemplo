8a61beb8a8596ff5f17dd1886d9143b9c7ed7b8d
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2021-06-05 17:52:41 -0500

    se documenta y prueba en ubuntu linux

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# latino-lib-ejemplo
-Ejemplo minimo para crear una libreria para latino en C
+## latino-lib-ejemplo
+Ejemplo minimo para crear una libreria para [latino](https://github.com/lenguaje-latino/latino) en C.
+En el archivo src/latino-libejemplo.c se encuentra comentado con lo minimo que se requiere.
+
+## Instalación
+
+### Linux / Mac
+
+#### Prerequisitos
+
+Tener instalado:
+[latino](https://github.com/lenguaje-latino/latino)
+[cmake](https://cmake.org/download/)
+
+Ejecutar lo siguiente en bash:
+
+```
+git clone https://github.com/lenguaje-latino/latino-lib-ejemplo
+cd latino-lib-ejemplo
+cmake .
+make
+sudo make install
+```
+
+### Windows
+
+#### Prerequisitos
+
+Tener instalado:
+[latino](https://github.com/lenguaje-latino/latino)
+[cmake](https://cmake.org/download/)
+[visual studio](https://visualstudio.microsoft.com/es/vs/community/)
+
+Ejecutar lo siguiente en cmd:
+
+```
+git clone https://github.com/lenguaje-latino/latino-lib-ejemplo
+cd latino-lib-ejemplo
+md build
+cd build
+cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release ..\
+```
+
+Abrir con visual studio 2019 y compilar la solucion latino-lib-ejemplo.sln
+Para instalar la libreria abrir visual studio con permisos de administrador
+Generar el proyecto de INSTALL.vcxproj
+
+### Uso de esta librería en código latino
+
+```
+// incluye el modulo en latino
+incluir("libejemplo")
+
+x = libejemplo.suma(1, 1.5)
+
+escribir(x)
+```
+
+ver ejemplo.lat
+
+### Ejecutar el ejemplo:
+`latino ejemplo.lat`
+
+
+### Dependecias
+
+Ninguna
+
+#### Cualquier aportación o sugerencia es bienvenida
---
+++ src/latino-libejemplo.c
@@ -0,0 +1,50 @@
+
+// Necesario para Windows
+#ifdef _WIN32
+#define LATINO_BUILD_AS_DLL
+#endif
+
+// incluir la cabecera de latino
+#include <latino.h>
+
+// se define el nombre de la libreria que se importara en latino
+// incluir("libejemplo")
+#define NOMBRE_LIB "libejemplo"
+
+// se crea una funcion static void que reciba la maquina virtual de latino
+static void lat_suma(lat_mv *mv) {
+    // desapila los parametros que recibe de latino
+    // estos vienen en orden inverso ej.
+    // suma(a, b)
+    // primero se desapila el valor de b
+    // postriormente se desapila el valor de a
+    lat_objeto *b = latC_desapilar(mv);
+    lat_objeto *a = latC_desapilar(mv);
+
+    // estos valores se recibe como objeto de latino
+    // por lo cual hay que convertilos a numeros con la funcion latC_checar_numerico
+    double _b = latC_checar_numerico(mv, b);
+    double _a = latC_checar_numerico(mv, a);
+
+    // hago cualquier operacion en C
+    double r = _a + _b;
+
+    // y para regresar el resultado a latino, lo tengo que apilar con funciones que inician con latC_apilar_*
+    latC_apilar_double(mv, r);
+}
+
+
+// Se crea la varibale para especificar las funciones que se exportarar a latino
+static const lat_CReg lib_ejemplo[] = {
+    // nombre_funcion_en_latino, nombre_funcion_c, numero_de_parametros
+    {"suma", lat_suma, 2}, 
+    // esta linea es necesaria para que detenga la importacion de funciones
+    {NULL, NULL, 0}
+};
+
+// El nombre de esta funcion debe de ser latC_abrir_liblatino_ seguido del nombre de la libreria en .dll o .so
+// En este caso definida en NOMBRE_LIB
+LATINO_API void latC_abrir_liblatino_libejemplo(lat_mv *mv) {
+  // Invocar a esta funcion para que cargue las funciones de C en latino
+  latC_abrir_liblatino(mv, NOMBRE_LIB, lib_ejemplo);
+}
